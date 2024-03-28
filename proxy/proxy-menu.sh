cb31b81348910573bdcdac585bfc0aa573557466
WJQSERVER <wjqserver@outlook.com>
2024-03-28 09:15:08 +0800

    0.9

--- proxy/proxy-menu.sh
+++ proxy/proxy-menu.sh
@@ -41,7 +41,21 @@ blue(){
 
 #BBR管理面板
 function bbr-manager(){
-wget -O bbr-manager.sh ${repo_url}systools/bbr-manager.sh && chmod +x bbr-manager.sh && ./bbr-manager.sh
+wget --no-check-certificate -O tcpx.sh https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcpx.sh
+chmod +x tcpx.sh
+./tcpx.sh
+
+#等待1s
+sleep 1
+
+#返回菜单/退出脚本
+read -p "是否返回菜单?: [Y/n]" choice
+
+if [[ "$choice" == "" || "$choice" == "Y" || "$choice" == "y" ]]; then
+    wget -O systools-menu.sh ${repo_url}systools/systools-menu.sh && chmod +x systools-menu.sh && ./systools-menu.sh
+else
+    echo "脚本结束"
+fi
 }
 
 #WARP
--- systools/network/bbr-manager.sh
+++ systools/network/bbr-manager.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 clear
-install wget
 wget --no-check-certificate -O tcpx.sh https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcpx.sh
 chmod +x tcpx.sh
 ./tcpx.sh
