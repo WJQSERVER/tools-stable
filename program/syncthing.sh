feead9048ccb528fe56c02df4c0b849e9bc1abba
WJQSERVER <wjqserver@outlook.com>
2024-05-10 08:47:16 +0800

    0.9

--- program/syncthing.sh
+++ program/syncthing.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-# 检查是否以root权限运行脚本
+# 检测脚本是否以 root 用户身份运行
 if [[ $EUID -ne 0 ]]; then
-    echo "请以root权限运行该脚本。"
-    exit 1
+   echo "错误：请使用 root 用户身份运行此脚本。"
+   exit 1
 fi
 
-# 安装Syncthing
-apt install syncthing
+apt-get install -y syncthing
 
-# 修改Syncthing地址
 PUBLIC_IP=0.0.0.0
 PORT=8384
 
-# 找到Syncthing的配置文件路径
-CONFIG_FILE=$(find /home/*/.config/syncthing/config.xml)
-
 # 修改地址为公网IP和指定端口
-sed -i "s|<address>.*</address>|<address>$PUBLIC_IP:$PORT</address>|" $CONFIG_FILE
+CONFIG_FILE="/root/.config/syncthing/config.xml"
+sed -i "s|<address>.*</address>|<address>$PUBLIC_IP:$PORT</address>|" "$CONFIG_FILE"
 
-cat <<EOF > /etc/systemd/system/syncthing.service
+cat > /etc/systemd/system/syncthing.service <<EOF
 [Unit]
-Description=Syncthing shell
+Description=Syncthing - Open Source Continuous File Synchronization
 After=network.target
 
 [Service]
-ExecStart=/root/data/syncthing.sh
-Restart=always
+ExecStart=/usr/bin/syncthing -no-browser
+Restart=on-failure
 User=root
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=default.target
+
 EOF
 
 # 启动Syncthing服务
@@ -38,20 +35,20 @@ systemctl daemon-reload
 systemctl enable syncthing
 systemctl start syncthing
 
-#回到root目录
+# 回到root目录
 cd /root
 
 # 导入配置文件
-source "repo_url.conf"
+source repo_url.conf
 
-#等待1s
+# 等待3s
 sleep 3
 
-#返回菜单/退出脚本
-read -p "是否返回菜单?: [Y/n]" choice
+# 返回菜单/退出脚本
+read -p "是否返回菜单？ [Y/n]" choice
 
 if [[ "$choice" == "" || "$choice" == "Y" || "$choice" == "y" ]]; then
-    wget -O program-menu.sh ${repo_url}program/program-menu.sh && chmod +x program-menu.sh && ./program-menu.sh
+    wget -O program-menu.sh "${repo_url}program/program-menu.sh" && chmod +x program-menu.sh && ./program-menu.sh
 else
     echo "脚本结束"
 fi
