905251363f520ed4f1f96da4243cb831cb8679c0
WJQSERVER <wjqserver@outlook.com>
2024-05-16 17:00:22 +0800

    0.9

--- systools/network/check_port_usage.sh
+++ systools/network/check_port_usage.sh
@@ -13,17 +13,35 @@ udp_result=$(sudo netstat -lnup | awk -v port="$port" '$1 ~ "udp" && $4 ~ ":'"$p
 
 # 输出结果
 if [[ -n "$tcp_result" ]]; then
-  echo "占用TCP端口 $port 的进程:"
+  echo "占用TCP 端口 $port 的进程:"
   echo "$tcp_result"
 else
-  echo "TCP端口 $port 未被占用."
+  echo "TCP 端口 $port 未被占用."
 fi
 
 echo
 
 if [[ -n "$udp_result" ]]; then
-  echo "占用UDP端口 $port 的进程:"
+  echo "占用UDP 端口 $port 的进程:"
   echo "$udp_result"
 else
-  echo "UDP端口 $port 未被占用."
+  echo "UDP 端口 $port 未被占用."
+fi
+
+#回到root目录
+cd /root
+
+# 导入配置文件
+source "repo_url.conf"
+
+#等待1s
+sleep 1
+
+#返回菜单/退出脚本
+read -p "是否返回菜单?: [Y/n]" choice
+
+if [[ "$choice" == "" || "$choice" == "Y" || "$choice" == "y" ]]; then
+    wget -O network_menu.sh ${repo_url}systools/network/network_menu.sh && chmod +x network_menu.sh && ./network_menu.sh
+else
+    echo "脚本结束"
 fi
