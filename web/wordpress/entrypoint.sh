f1dd4ef995c1b96e4f984ca15f839fdc9bf052b0
WJQSERVER <wjqserver@outlook.com>
2024-05-22 12:57:32 +0800

    0.9

---
+++ web/wordpress/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+siteurl=$siteurl
+
+sqlite3 "/var/www/html/wordpress/wp-content/database/.ht.sqlite" <<EOF
+UPDATE wp_options SET option_value = '$siteurl' WHERE option_name = 'siteurl';
+UPDATE wp_options SET option_value = '$siteurl' WHERE option_name = 'home';
+.quit
+EOF
