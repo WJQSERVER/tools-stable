10d7a0da7719be53af35b758ad7fd321c0e216fc
WJQSERVER <wjqserver@outlook.com>
2024-05-22 18:49:37 +0800

    0.9

---
+++ web/wordpress/init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ ! -f /data/caddy/config/Caddyfile ]; then
+    cp /data/caddy/Caddyfile /data/caddy/config/Caddyfile
+fi
+
+if [ ! -f /var/www/html/wordpress/index.php ]; then
+    cp -r /var/www/html/wordpress.d/* /var/www/html/wordpress/
+fi
+if [ ! -f /data/caddy/config.d/wordpress ]; then
+    cat > /data/caddy/config.d/wordpress <<EOF
+    
+$siteurl {
+    root * /var/www/html/wordpress
+    file_server
+    php_fastcgi unix//run/php/php8.2-fpm.sock {
+        import header_realip
+    }
+    import log wordpress
+    import error_page
+    import encode  
+    import cache 0s 200s
+}
+
+EOF
+
+fi
+
+chmod 755 -R /var/www/html/wordpress 
+chmod 640 /var/www/html/wordpress/wp-content/database/.ht.sqlite 
+chown www-data:www-data -R /var/www/html/wordpress
+
+sqlite3 "/var/www/html/wordpress/wp-content/database/.ht.sqlite" <<EOF
+UPDATE wp_options SET option_value = '$siteurl' WHERE option_name = 'siteurl';
+UPDATE wp_options SET option_value = '$siteurl' WHERE option_name = 'home';
+.quit
+EOF
+
+/data/caddy/caddy run --config /data/caddy/config/Caddyfile
