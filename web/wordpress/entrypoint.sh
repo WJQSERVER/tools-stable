1a1be0d48b87bb73920ae01cfb4c542632bb529e
WJQSERVER <wjqserver@outlook.com>
2024-05-22 13:53:17 +0800

    0.9

--- web/wordpress/entrypoint.sh
+++ web/wordpress/entrypoint.sh
@@ -7,3 +7,16 @@ UPDATE wp_options SET option_value = '$siteurl' WHERE option_name = 'siteurl';
 UPDATE wp_options SET option_value = '$siteurl' WHERE option_name = 'home';
 .quit
 EOF
+
+cat > /data/caddy/config/wordpress <<EOF
+$siteurl {
+	root * /var/www/html/wordpress
+    php_fastcgi unix//run/php/php8.2-fpm.sock {
+        import header_realip
+    }
+    file_server
+    import log
+    import error_page  
+    import encode
+}
+EOF
